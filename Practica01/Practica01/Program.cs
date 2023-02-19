ed2adc481cc15c210d1a9c0b88812a4a78c3fe61
Luis Vasquez <vluis5457@gmail.com>
2023-02-19 01:45:19 -0600

    Practica 01- Terminada

--- Practica01/Practica01/Program.cs
+++ Practica01/Practica01/Program.cs
@@ -1,8 +1,19 @@
+using Microsoft.EntityFrameworkCore;
+using Practica01.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+//TODO: Configurar conexion
+const string CONNETION_NAME = "EquiposDB";
+var connection_string = builder.Configuration.GetConnectionString(CONNETION_NAME);
+
+builder.Services.AddDbContext<EquipoContext>(options => options.UseSqlServer(connection_string)); 
+
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
